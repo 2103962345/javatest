b4d213490051cd9347f15e844599553488839051
2103962345 <2103962345@mail.ru>
2020-05-19 16:10:23 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Invoke-Expression "javac $PSScriptRoot\HelloWorld.java"
-Invoke-Expression "java $PSScriptRoot\HelloWorld"
+Invoke-Expression "C:\Program Files\Java\jdk1.8.0_241\bin\javac $PSScriptRoot\HelloWorld.java"
+Invoke-Expression "C:\Program Files\Java\jdk1.8.0_241\bin\java.exe $PSScriptRoot\HelloWorld"
 #Start-Process -FilePath javac -ArgumentList "$PSScriptRoot\HelloWorld.java"
 #Start-Process -FilePath java -ArgumentList "$PSScriptRoot\HelloWorld"

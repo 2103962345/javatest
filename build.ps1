adae7b9cf534bfe237f9adab28485d1787e05778
2103962345 <2103962345@mail.ru>
2020-05-19 16:45:09 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
 
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
-$path1 = "C:\Program Files.exeJava\jdk1.8.0_241\bin\javac";
-$path2 = "C:\Program Files\Java\jdk1.8.0_241\bin\java.exe";
+#$path1 = "C:\Program Files.exeJava\jdk1.8.0_241\bin\javac";
+#$path2 = "C:\Program Files\Java\jdk1.8.0_241\bin\java.exe";
 Write-Host "Congratulations! Your first script executed successfully"
-Invoke-Expression "$path1 $PSScriptRoot\HelloWorld.java"
-Invoke-Expression "$path2 $PSScriptRoot\HelloWorld"
-#Start-Process -FilePath javac -ArgumentList "$PSScriptRoot\HelloWorld.java"
-#Start-Process -FilePath java -ArgumentList "$PSScriptRoot\HelloWorld"
+#Invoke-Expression "$path1 $PSScriptRoot\HelloWorld.java"
+#Invoke-Expression "$path2 $PSScriptRoot\HelloWorld"
+Start-Process -FilePath javac -ArgumentList "$PSScriptRoot\HelloWorld.java"
+Start-Process -FilePath java -ArgumentList "$PSScriptRoot\HelloWorld"

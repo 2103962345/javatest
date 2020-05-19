2d0c541c9082f1553678a22551d4af3168fadd2d
2103962345 <2103962345@mail.ru>
2020-05-19 17:46:11 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
 
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
-$path1 = "C:\Program Files\Java\jdk1.8.0_241\bin\javac.exe";
-$path2 = "C:\Program Files\Java\jdk1.8.0_241\bin\java.exe";
+$path1 = 'C:\Program Files\Java\jdk1.8.0_241\bin\javac.exe';
+$path2 = 'C:\Program Files\Java\jdk1.8.0_241\bin\java.exe';
 Write-Host "Congratulations! Your first script executed successfully"
 Invoke-Expression "$path1 -version"
 Invoke-Expression "$path2 -version"

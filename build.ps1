a12cf5fdbb277e494758008b5c3c41e2324096fd
2103962345 <2103962345@mail.ru>
2020-05-19 17:13:31 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,5 +6,5 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 Write-Host "Congratulations! Your first script executed successfully"
 #Invoke-Expression "$path1 $PSScriptRoot\HelloWorld.java"
 #Invoke-Expression "$path2 $PSScriptRoot\HelloWorld"
-Start-Process -FilePath "C:\Program Files\Java\jdk1.8.0_241\bin\javac.exe" -ArgumentList "$PSScriptRoot\HelloWorld.java" - NoNewWindow
+Start-Process -FilePath "C:\Program Files\Java\jdk1.8.0_241\bin\javac.exe" -ArgumentList "$PSScriptRoot\HelloWorld.java" -NoNewWindow
 Start-Process -FilePath "C:\Program Files\Java\jdk1.8.0_241\bin\java.exe" -ArgumentList "$PSScriptRoot\HelloWorld" -NoNewWindow

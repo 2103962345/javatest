2b61c8c959bdde580dd062a8ca27612bff7e3220
2103962345 <2103962345@mail.ru>
2020-05-20 13:21:32 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -8,5 +8,5 @@ Start-Process -FilePath javac -ArgumentList "HelloWorld.java" -NoNewWindow
 Start-Process -FilePath javac -ArgumentList "-version" -NoNewWindow
 #Start-Process -FilePath java -ArgumentList "-classpath HelloWorld" -NoNewWindow
 
-Start-Process java -ArgumentList "-classpath . HelloWorld"  -NoNewWindow #`
+Start-Process java -ArgumentList '-cp', '.', 'HelloWorld'  -NoNewWindow #`
 #-RedirectStandardOutput '.\console.out' -RedirectStandardError '.\console.err' 
